da3f03b3150fbc345a9757f1f59250527567754f
Anthony <anthony@preeminentproductions.com>
2014-06-26 20:15:59 -0700

    Finish Sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ end
   def create
   	@user = User.new(user_params)
   	  if @user.save
+        sign_in @user
   	  	flash[:success] = "Welcome to my Awesome Sample App!"
   	  	redirect_to @user
   	  else
