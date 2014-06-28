f7b7fbbff8be606909058bb6497ae4f2d8a73ffc
Anthony <anthony@preeminentproductions.com>
2014-06-27 19:23:42 -0700

    add user following-finish app

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,23 @@
 HartlApp::Application.routes.draw do
-  root 'static_pages#home'
-  resources :users
+  
+resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
+ 
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
+
+root 'static_pages#home'
+
   match '/signup',  to: 'users#new',            via: 'get'
-  match '/users',  to: 'users#index',            via: 'get'
+  
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
-  match '/signup', to: 'users#new', via: 'get'
+  
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
